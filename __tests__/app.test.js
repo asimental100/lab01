513787676811d6191bbfbe7e47fdd1e5113a5292
Adrian Simental <asimental100@hotmail.com>
2020-09-10 20:29:36 -0700

    first copyAndPush test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,8 @@ const pool = require('../lib/utils/pool');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
-const getName = require('../lib/functions');
+const getName = require('../lib/getName');
+const copyAndPush = require('../lib/copyAndPush');
 
 
 describe('lab01 routes', () => {
@@ -14,6 +15,14 @@ describe('lab01 routes', () => {
   it('should return the name from an object', async() => {
     const testObject = { name: 'craig', age: 55 };
     const name = await getName(testObject);
+
     expect(name).toEqual('craig');
+  });
+
+  it('should copy original array and push new item into new array', async() => {
+    const array = [1, 2, 3];
+    const newArray = await copyAndPush(array, 4);
+
+    expect(newArray).toEqual([1, 2, 3, 4]);
   });
 });
